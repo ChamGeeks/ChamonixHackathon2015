3ead6e6bf266dce3c40b0c407afc8e268b5b0d58
njp <n.pearman@gmail.com>
2015-10-16 23:21:14 +0200

    added days of week

--- api.rb
+++ api.rb
@@ -8,17 +8,30 @@ end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/burgers_and_beers.db")
 
+class DayOfWeek
+  include DataMapper::Resource
+
+  storage_names[:default] = 'days_of_week'
+
+  property :id, Serial
+  property :name, String
+end
+
 class Bar
   include DataMapper::Resource
 
   property :id, Serial
   property :name, String
+  property :latitude, Decimal
+  property :longitude, Decimal
+
 end
 
 DataMapper.finalize
 
 
 get '/' do
-  "Bar count: #{Bar.all.size}"
+  "Bars: #{Bar.all.map{|b| b.name}.join(', ')}<br />"\
+  "Days of week: #{DayOfWeek.all.map{|d| d.name}.join(', ')}"
 end
 
