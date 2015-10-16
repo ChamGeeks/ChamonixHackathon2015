c2faf375e461449f3b35e79c4b003a9dbaa4b9cc
njp <n.pearman@gmail.com>
2015-10-16 22:51:53 +0200

    added hook up for bars into the db

--- api.rb
+++ api.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/burgers_and_beers.db")
+
+class Bar
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+end
+
+DataMapper.finalize
+
 
 get '/' do
-  'pingpingping'
+  "Bar count: #{Bar.all.size}"
 end
 
